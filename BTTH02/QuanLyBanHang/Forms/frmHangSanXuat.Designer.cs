5dcbe424a65ba803045cc317dd4bda471a1cf608
dth235698-nguyendominh <minh_dth235698@student.agu.edu.vn>
2026-01-30 23:10:00 +0700

    nộp BTTH02

---
+++ BTTH02/QuanLyBanHang/Forms/frmHangSanXuat.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace QuanLyBanHang.Forms
+{
+    partial class frmHangSanXuat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox2 = new GroupBox();
+            dataGridView = new DataGridView();
+            codeID = new DataGridViewTextBoxColumn();
+            TenHangSanXuat = new DataGridViewTextBoxColumn();
+            groupBox1 = new GroupBox();
+            btnThoat = new Button();
+            btnHuyBo = new Button();
+            btnLuu = new Button();
+            btnXoa = new Button();
+            btnSua = new Button();
+            btnThem = new Button();
+            txtTenHangSanXuat = new TextBox();
+            label1 = new Label();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView).BeginInit();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(dataGridView);
+            groupBox2.Location = new Point(21, 132);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(758, 308);
+            groupBox2.TabIndex = 3;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Danh sách hãng sản xuất";
+            // 
+            // dataGridView
+            // 
+            dataGridView.AllowUserToAddRows = false;
+            dataGridView.AllowUserToDeleteRows = false;
+            dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView.Columns.AddRange(new DataGridViewColumn[] { codeID, TenHangSanXuat });
+            dataGridView.Dock = DockStyle.Fill;
+            dataGridView.Location = new Point(3, 23);
+            dataGridView.MultiSelect = false;
+            dataGridView.Name = "dataGridView";
+            dataGridView.RowHeadersWidth = 51;
+            dataGridView.Size = new Size(752, 282);
+            dataGridView.TabIndex = 0;
+            // 
+            // codeID
+            // 
+            codeID.DataPropertyName = "ID";
+            codeID.HeaderText = "ID";
+            codeID.MinimumWidth = 6;
+            codeID.Name = "codeID";
+            // 
+            // TenHangSanXuat
+            // 
+            TenHangSanXuat.DataPropertyName = "TenHangSanXuat";
+            TenHangSanXuat.HeaderText = "Tên hãng sản xuất";
+            TenHangSanXuat.MinimumWidth = 6;
+            TenHangSanXuat.Name = "TenHangSanXuat";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(btnThoat);
+            groupBox1.Controls.Add(btnHuyBo);
+            groupBox1.Controls.Add(btnLuu);
+            groupBox1.Controls.Add(btnXoa);
+            groupBox1.Controls.Add(btnSua);
+            groupBox1.Controls.Add(btnThem);
+            groupBox1.Controls.Add(txtTenHangSanXuat);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(21, 11);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(758, 115);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Thông tin hãng sản xuất";
+            // 
+            // btnThoat
+            // 
+            btnThoat.Location = new Point(604, 61);
+            btnThoat.Name = "btnThoat";
+            btnThoat.Size = new Size(80, 29);
+            btnThoat.TabIndex = 7;
+            btnThoat.Text = "Thoát";
+            btnThoat.UseVisualStyleBackColor = true;
+            btnThoat.Click += btnThoat_Click;
+            // 
+            // btnHuyBo
+            // 
+            btnHuyBo.Location = new Point(518, 61);
+            btnHuyBo.Name = "btnHuyBo";
+            btnHuyBo.Size = new Size(80, 29);
+            btnHuyBo.TabIndex = 6;
+            btnHuyBo.Text = "Huỷ Bỏ";
+            btnHuyBo.UseVisualStyleBackColor = true;
+            btnHuyBo.Click += btnHuyBo_Click;
+            // 
+            // btnLuu
+            // 
+            btnLuu.Location = new Point(432, 61);
+            btnLuu.Name = "btnLuu";
+            btnLuu.Size = new Size(80, 29);
+            btnLuu.TabIndex = 5;
+            btnLuu.Text = "Lưu";
+            btnLuu.UseVisualStyleBackColor = true;
+            btnLuu.Click += btnLuu_Click;
+            // 
+            // btnXoa
+            // 
+            btnXoa.Location = new Point(346, 61);
+            btnXoa.Name = "btnXoa";
+            btnXoa.Size = new Size(80, 29);
+            btnXoa.TabIndex = 4;
+            btnXoa.Text = "Xoá";
+            btnXoa.UseVisualStyleBackColor = true;
+            btnXoa.Click += btnXoa_Click;
+            // 
+            // btnSua
+            // 
+            btnSua.Location = new Point(260, 61);
+            btnSua.Name = "btnSua";
+            btnSua.Size = new Size(80, 29);
+            btnSua.TabIndex = 3;
+            btnSua.Text = "Sửa";
+            btnSua.UseVisualStyleBackColor = true;
+            btnSua.Click += btnSua_Click;
+            // 
+            // btnThem
+            // 
+            btnThem.Location = new Point(174, 61);
+            btnThem.Name = "btnThem";
+            btnThem.Size = new Size(80, 29);
+            btnThem.TabIndex = 2;
+            btnThem.Text = "Thêm";
+            btnThem.UseVisualStyleBackColor = true;
+            btnThem.Click += btnThem_Click;
+            // 
+            // txtTenHangSanXuat
+            // 
+            txtTenHangSanXuat.Location = new Point(174, 28);
+            txtTenHangSanXuat.Name = "txtTenHangSanXuat";
+            txtTenHangSanXuat.Size = new Size(566, 27);
+            txtTenHangSanXuat.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(16, 35);
+            label1.Name = "label1";
+            label1.Size = new Size(150, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Tên hãng sản xuất (*):";
+            // 
+            // frmHangSanXuat
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "frmHangSanXuat";
+            Text = "Hãng Sản Xuất";
+            Load += frmHangSanXuat_Load_1;
+            groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox2;
+        private DataGridView dataGridView;
+        private GroupBox groupBox1;
+        private Button btnThoat;
+        private Button btnHuyBo;
+        private Button btnLuu;
+        private Button btnXoa;
+        private Button btnSua;
+        private Button btnThem;
+        private TextBox txtTenHangSanXuat;
+        private Label label1;
+        private DataGridViewTextBoxColumn codeID;
+        private DataGridViewTextBoxColumn TenHangSanXuat;
+    }
+}
